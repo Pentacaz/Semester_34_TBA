cb4b55bf258fed3c51a50d96db1f0082fb71ef50
Emilia <leonieengelbach@googlemail.com>
2025-02-17 19:29:04 +0100

    dungeon

---
+++ Assets/+++Work_Data/Scripts/Transport.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Transport : MonoBehaviour
+{
+ public List<Transform> spawnPoints;
+
+
+ private void OnTriggerEnter(Collider other)
+ {
+     if (other.CompareTag("Player"))
+          {
+             
+              
+              int index = Random.Range(0, spawnPoints.Count);
+            //other.gameObject.transform.position
+             
+          }
+ }
+}
